7d821d1ae3eda3235a0e1f372d65733c01359c29
Samuel Cedeño Quinde <Trollmex_CQ@hotmail.com>
2024-01-11 17:03:28 -0500

    Agregue el registro_boleto.html

--- app/app.py
+++ app/app.py
@@ -5,6 +5,9 @@ app = Flask (__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/registro')
+def registro():
+    return render_template('Registro_boleto.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
