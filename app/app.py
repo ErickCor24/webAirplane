59e7f35204dbafc7fe25e9ec2532ae16d68ccf78
DanRyzer10 <angeldaniel6709@gmail.com>
2024-01-11 17:13:08 -0500

    restyled components

--- app/app.py
+++ app/app.py
@@ -6,6 +6,11 @@ app = Flask (__name__)
 def index():
     return render_template('index.html')
 
+
+@app.route('/flight')
+def flight():
+    return render_template('crear_vuelo.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
 
