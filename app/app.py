0121f2bb153efa63be1cb1cf95de306f3398135c
Drastico <oliverveas34@gmail.com>
2024-01-11 19:12:52 -0500

    guardando cambios nuevos

--- app/app.py
+++ app/app.py
@@ -6,6 +6,10 @@ app = Flask (__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/vuelo')
+def vuelo():
+    return render_template('crear_vuelo.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
 
