9b26901365d09bc47d4c324b5d0eef3381c4e2e9
DanRyzer10 <angeldaniel6709@gmail.com>
2024-01-18 09:46:21 -0500

    added flight routes

--- app/models/vuelo.py
+++ app/models/vuelo.py
@@ -1,12 +1,20 @@
 from sqlalchemy import Sequence
+from sqlalchemy import Time
+from sqlalchemy import Date
 from utils.db import db
 
 
 class Vuelo(db.Model):
     id= db.Column(db.Integer, Sequence('incrementidvuelo',start=1,increment=1),primary_key=True)
-    fecha_salida= db.Column(db.Date, nullable=False)
-    hora_salida= db.Column(db.Time, nullable=False)
+    fecha_salida= db.Column(db.String(10), nullable=False)
+    hora_salida= db.Column(db.String(10), nullable=False)
     precio= db.Column(db.Float, nullable=False)
-    estado= db.Column(db.Boolean, nullable=False)
-    id_aeropuerto_origen = db.Column(db.Integer, db.ForeignKey('aeropuerto.id'), nullable=False)
-    id_avion = db.Column(db.Integer, db.ForeignKey('avion.id'), nullable=False)
+    id_aeropuerto_origen = db.Column(db.String(7), db.ForeignKey('aeropuerto.id'), nullable=False)
+    id_avion = db.Column(db.String(7), db.ForeignKey('avion.id'), nullable=False)
+
+    def __init__(self, fecha_salida, hora_salida, precio, id_aeropuerto_origen, id_avion):
+        self.fecha_salida = fecha_salida
+        self.hora_salida = hora_salida
+        self.precio = precio
+        self.id_aeropuerto_origen = id_aeropuerto_origen
+        self.id_avion = id_avion
--- app/routes/vuelo.py
+++ app/routes/vuelo.py
@@ -1,6 +1,9 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,request,redirect,url_for
+from utils.db import db
 from models.avion import Avion
 from models.aeropuerto import Aeropuerto
+from models.vuelo import Vuelo
+from datetime import datetime,time
 
 vuelo = Blueprint('vuelo', __name__, template_folder='templates',url_prefix='/vuelo')
 
@@ -9,3 +12,24 @@ def register():
     aviones= Avion.query.all()
     aeropuertos = Aeropuerto.query.all()
     return render_template('views/vuelo/register.html', aviones=aviones, aeropuertos=aeropuertos)
+
+@vuelo.route('/', methods=['GET','POST'])
+def store():
+    if request.method == 'POST':
+        fecha_salida = request.form['fecha_salida']
+        hora_salida  = request.form['hora_salida']
+        precio = request.form['precio']
+        avion_id = request.form['avion_asignado']
+        aeropuerto_id= request.form['aeropuerto_destino']
+
+        try:
+            vuelo = Vuelo(fecha_salida,hora_salida,precio,aeropuerto_id,avion_id)
+            precio = float(precio)
+            db.session.add(vuelo,)
+            db.session.commit()
+        except Exception as e:
+            print(e)
+    return redirect(url_for('vuelo.register'))
+
+        
+
